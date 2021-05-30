504daeb59294bf5a13695e1bfb8d725af3b82e90
Madhwan Phadtare <madhwanphadtere.123@gmail.com>
2021-05-30 20:44:36 +0530

    intro hello

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,11 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: MaterialApp(
-        home: Column(
-          children: [Text("data1"), Text("data2")],
+    return MaterialApp(
+      home: Align(
+        alignment: Alignment.center,
+        child: Column(
+          children: [Text("data 1"), Text("data 2")],
         ),
       ),
     );
