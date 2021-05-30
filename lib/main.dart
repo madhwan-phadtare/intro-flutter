7d4a8669861294cf17757354d20f6c5ab6709263
Madhwan Phadtare <madhwanphadtere.123@gmail.com>
2021-05-30 18:45:33 +0530

    intro 1.0

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child : Column()
+      child: MaterialApp(
+        home: Column(
+          children: [Text("data1"), Text("data2")],
+        ),
+      ),
     );
   }
-}  
+}
